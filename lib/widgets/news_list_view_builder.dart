ed952667dfcffef43f15c67b2b0623af1427f58f
SAHİR ALZAKARIA <sahir.alzakaria@gmail.com>
2025-07-09 18:06:40 +0300

    code refactor

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,4 +1,3 @@
-import 'package:app06/models/article_model.dart';
 import 'package:app06/services/news_service.dart';
 import 'package:app06/widgets/news_list_view.dart';
 import 'package:dio/dio.dart';
@@ -12,7 +11,21 @@ class NewsListViewBuilder extends StatelessWidget {
     return FutureBuilder(
       future: NewsService(Dio()).getNews(),
       builder: (context, snapshot) {
-        return NewsListView(articles: snapshot.data ?? []);
+        if (snapshot.hasData) {
+          return NewsListView(articles: snapshot.data!);
+        } else if (snapshot.hasError) {
+          return SliverToBoxAdapter(
+            child: Text(
+              'OOPS , News Not Found !!',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          );
+        } else {
+          return SliverToBoxAdapter(
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
       },
     );
   }
