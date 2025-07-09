06837810c01d37eeb19c7e8c4d0eb92f3bc36a56
SAHİR ALZAKARIA <sahir.alzakaria@gmail.com>
2025-07-09 17:57:48 +0300

    Fix some bugs

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -4,22 +4,15 @@ import 'package:app06/widgets/news_list_view.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
+class NewsListViewBuilder extends StatelessWidget {
   const NewsListViewBuilder({super.key});
-
-  @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  List<ArticleModel> articles = [];
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: NewsService(Dio()).getNews(),
       builder: (context, snapshot) {
-        return NewsListView(articles: articles);
+        return NewsListView(articles: snapshot.data ?? []);
       },
     );
   }
